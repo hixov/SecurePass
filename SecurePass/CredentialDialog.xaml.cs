bbcbc431cd3c9ac719114cbebe26b770e88a6693
hixov <lennicovenoor@gmail.com>
2025-08-12 12:58:13 +0300

    Add files via upload

---
+++ SecurePass/CredentialDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Windows;
+using static SecurePassWPF.MainWindow;
+
+namespace SecurePassWPF
+{
+    public partial class CredentialDialog : Window
+    {
+        public Credential NewCredential { get; private set; }
+
+        public CredentialDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(ServiceTextBox.Text))
+            {
+                MessageBox.Show("Имя сервиса не может быть пустым.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            NewCredential = new Credential
+            {
+                Service = ServiceTextBox.Text,
+                Username = UsernameTextBox.Text,
+                Password = PasswordTextBox.Text
+            };
+
+            DialogResult = true;
+        }
+    }
+}
